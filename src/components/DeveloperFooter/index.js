9bd25742ed2a52d3d941e3ba8cca0b3008d55184
obitrim <pevangelist516@gmail.com>
2021-08-18 22:41:05 +0000

    feature: created developer's footer component

---
+++ src/components/DeveloperFooter/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import "./DeveloperFooter.css";
+import Container from "../Container";
+import profileImg from "../../assets/images/profile.jpg";
+
+const DeveloperFooter = (props) => {
+  return (
+    <footer className="DeveloperFooter">
+    	<Container>
+    		<div className="DeveloperFooter__Profile">
+    			<div className="ImgBox">
+    				<img src={profileImg} alt="developer's image" />
+    			</div>
+    		</div>
+    		<article className="DeveloperFooter__Info">
+    			<h2>About Developer</h2>
+    			<p>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Deleniti, nobis.</p>
+    			<p>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Deleniti, nobis. icing elit. Deleniti, nobis lorem5 </p>
+    			<strong>
+    				<span>Have a work for me?</span>&nbsp;
+    				<a href="#" className="EmailMeBtn">Email Me</a>
+    			</strong>
+    			<div className="SocialMedia">
+    				<a href="#">Facebook</a>&nbsp;.&nbsp;
+    			    <a href="#">Twitter</a>&nbsp;.&nbsp;
+    			    <a href="#">LinkedIn</a>
+    			</div>
+    		</article>
+    	</Container>
+    </footer>
+  )
+}
+
+export default DeveloperFooter;
