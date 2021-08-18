5e6ba8fce4d384ec136211d98d78b1595e7da448
obitrim <pevangelist516@gmail.com>
2021-08-18 22:42:55 +0000

    feature: route & page setup
    - import route components\n-import global styles\n- add template

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import AppHome from "../AppHome";
 
 const App = (props) => {
   return (
-    <h1>App</h1>
+    <Switch>
+    	<Route path="/" exact component={AppHome} />
+    </Switch>
   )
 }
 
