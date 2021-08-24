cc21653540ae43ec31f2009a0c3d8b089bf6ac87
obitrim <pevangelist516@gmail.com>
2021-08-24 23:00:11 +0000

    feature: refactored LayeredButton

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import "./Banner.css";
 import Container from "../Container";
+import LayeredButton from "../Buttons/LayeredButton";
 import chartImg from "../../assets/images/mobile-finance.png";
 
 const Banner = (props) => {
@@ -12,9 +13,11 @@ const Banner = (props) => {
     			<h1>Know and Track</h1>
     			<h2>Your spending habit</h2>
     			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dolore provident iure repellat magnam consequatur.</p>
-    			<button type="button" className="LayeredButton LayeredButton--lg">
-    				<span>Start Tracking</span>
-    			</button>
+    			<LayeredButton 
+                    type="button" 
+                    text="Start Tracking" 
+                    className="LayeredButton--lg" 
+                />
     		</article>
     		<div className="Banner__ChartWrapper">
     			<img src={chartImg} className="Banner__ChartWrapper__Img" alt="3d charts on mobile mockup" />
