53cb39e134837391e0344c34cd57d000944f5aa9
obitrim <pevangelist516@gmail.com>
2021-08-24 22:03:20 +0000

    feature: create view component for protected pages

---
+++ src/pages/Protected/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from "../../components/AppHeader";
+
+const ProtectedViews = (props) => {
+  return (
+    <section>
+    	<AppHeader />
+    	<section>Main</section>
+    </section>
+  )
+}
+
+export default ProtectedViews;
