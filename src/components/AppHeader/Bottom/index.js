0c4a4d866213b40b9644460981371df733362c0f
obitrim <pevangelist516@gmail.com>
2021-08-24 23:01:19 +0000

    feature: added tab template

--- src/components/AppHeader/Bottom/index.js
+++ src/components/AppHeader/Bottom/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import "./Bottom.css";
+import Container from "../../Container";
+
 const Bottom = (props) => {
   return (
-    <div>Bottom</div>
+    <Container className="HeaderBottom">
+    	<button className="HeaderBottom__Tab HeaderBottom__Tab--Active">
+    		Dashboard
+    		<span></span>
+    		<span></span>
+    	</button>
+    	<button className="HeaderBottom__Tab">
+    		Accounts
+    		<span></span>
+    		<span></span>
+    	</button>
+    	<button className="HeaderBottom__Tab">
+    		Loans
+    		<span></span>
+    		<span></span>
+    	</button>
+    	<button className="HeaderBottom__Tab">
+    		Learn
+    		<span></span>
+    		<span></span>
+    	</button>
+    </Container>
   )
 }
 
