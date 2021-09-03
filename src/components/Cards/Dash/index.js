be08334ad81876ac72221083793d20c23bc6d16f
obitrim <pevangelist516@gmail.com>
2021-09-03 15:00:51 +0000

    feature: created a dash card

---
+++ src/components/Cards/Dash/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BaseCard from "../BaseCard.js"
+
+const Dash = (props) => {
+  return (
+   	<BaseCard className="DashCard">
+   		some data
+   	</BaseCard>
+  )
+}
+
+export default Dash;
