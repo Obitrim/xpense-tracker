69a2b00e383cfeca77c72a07d5eac361cf4f8254
obitrim <pevangelist516@gmail.com>
2021-09-08 19:15:10 +0000

    feature: props
    configured to receive flat and radius props

--- src/components/Cards/BaseCard.js
+++ src/components/Cards/BaseCard.js
@@ -3,21 +3,28 @@ import PropTypes from "prop-types";
 
 import "./BaseCard.css";
 
-const Card = ({children, className, flat, ...props}) => {
+const Card = ({children, radius, className, flat, ...props}) => {
   return (
-    <article className={`BaseCard ${flat && "BaseCard--Flat"} ${className}`}>{children}</article>
+    <article 
+    	className={`BaseCard ${flat && "BaseCard--Flat"} ${className}`}
+    	style={{
+    		borderRadius: radius
+    	}}
+    >{children}</article>
   )
 }
 
 Card.propTypes = {
 	children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
 	className: PropTypes.string,
-	flat: PropTypes.bool
+	flat: PropTypes.bool,
+	radius: PropTypes.string
 }
 
 Card.defaultProps = {
 	className: "",
-	flat: false
+	flat: false,
+	radius: "5px"
 }
 
 export default Card;
