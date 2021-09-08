667685bb2671cd3293cb21062c09958507a9e4a1
obitrim <pevangelist516@gmail.com>
2021-09-08 19:13:09 +0000

    feature: created metrices and transactions sections

--- src/pages/Protected/Dashboard/index.js
+++ src/pages/Protected/Dashboard/index.js
@@ -1,16 +1,97 @@
 import React from 'react';
+import Graph from "../../../components/GraphCanvas";
 import DashCard from "../../../components/Cards/Dash";
+import BaseCard from "../../../components/Cards/BaseCard";
+
+import "./Dashboard.css";
 
 const Dashboard = (props) => {
   return (
   	<>
-  		<section className="Dashboard__Dashes">
+  		<h1 className="Dashboard__Title">Dashboard</h1>
+  		<section className="Dashboard__Section Dashboard__Dashes">
 	    	<DashCard />
 	    	<DashCard />
 	    	<DashCard />
 	    </section>
 	    {/*Graph*/}
-	    {/*Accounts*/}
+  		<h1 className="Dashboard__Title">Statistics</h1>
+  		<div className="Dashboard__FilterBar">
+  			<button className="Active">Today</button>
+  			<button>Weekly</button>
+  			<button>Monthly</button>
+  			<button>Yearly</button>
+  		</div>
+	    <section className="Dashboard__Section Dashboard__Stats">
+	    	<Graph />
+	    	<Graph />
+	    </section>
+	    {/*Transactions*/}
+  		<h1 className="Dashboard__Title">Transactions</h1>
+  		<BaseCard className="Dashboard__Section TableResponsive">
+  			<table>
+	  			<thead>
+	  				<tr>
+	  					<th>header</th>
+	  					<th>header</th>
+	  					<th>header</th>
+	  					<th>header</th>
+	  					<th>header</th>
+	  				</tr>
+	  			</thead>
+	  			<tbody>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  				<tr>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  					<td>data</td>
+	  				</tr>
+	  			</tbody>
+	  		</table>
+  		</BaseCard>
   	</>
   )
 }
