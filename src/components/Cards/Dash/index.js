426dcd08cb5b5f95887aff6ca5bf58e2a02e569c
obitrim <pevangelist516@gmail.com>
2021-09-08 19:14:32 +0000

    feature: added markup

--- src/components/Cards/Dash/index.js
+++ src/components/Cards/Dash/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import BaseCard from "../BaseCard.js"
+import "./Dash.css";
 
 const Dash = (props) => {
   return (
    	<BaseCard className="DashCard">
-   		some data
+   		<h2 className="DashCard__Title">Account Balance</h2>
+   		<p className="DashCard__Value">GH$ <span>3000</span></p>
+   		<p className="DashCard__Info">
+   			<strong>Income:</strong> GH$3300&nbsp;-&nbsp; 
+   			<strong>Expense:</strong> Gh$3000</p>
    	</BaseCard>
   )
 }
